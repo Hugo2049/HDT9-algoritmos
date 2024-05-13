3302b9c3eece685c48b761a295aa00e7ac351ca0
Hb_W <hugobarillasvi@hotmail.com>
2024-05-12 20:14:21 -0600

    encontrar ruta corta
    implementar función para hallar la ruta más corta

--- gestor_rutas.py
+++ gestor_rutas.py
@@ -9,3 +9,12 @@ def crear_grafo():
             costo = int(costo)
             grafo.add_edge(inicio.strip(), fin.strip(), weight=costo)
     return grafo
+
+# Función para encontrar las rutas más cortas utilizando Dijkstra
+def encontrar_rutas(grafo, inicio):
+    rutas = nx.single_source_dijkstra_path(grafo, inicio)
+    costos = nx.single_source_dijkstra_path_length(grafo, inicio)
+    for destino, ruta in rutas.items():
+        if destino != inicio:
+            costo_total = costos[destino]
+            print(f"Desde {inicio} hasta {destino}: {ruta}, Costo total: {costo_total}")
